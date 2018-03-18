a9bc2b15bd4774b2cb2789c4338f988b8697c8ad
danielgomez <aka.mdsg@gmail.com>
2018-03-18 10:23:44 +0000

    ocn: use latest ocnwhl fingerprint for all variants
    Change-Id: I4cb6e2a7e962fae678088688e03ded754448d626

--- slim.mk
+++ slim.mk
@@ -13,3 +13,9 @@ PRODUCT_NAME := slim_ocn
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := ocn
 PRODUCT_MANUFACTURER := htc
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRODUCT_NAME=ocnwhl_00617 \
+        PRIVATE_BUILD_DESC="ocnwhl_00617-user 8.0.0 OOPR6.170623.013 1011554.1 release-keys"
+
+BUILD_FINGERPRINT := htc/ocnwhl_00617/htc_ocnwhl:8.0.0/OPR6.170623.013/1011554.1:user/release-keys
