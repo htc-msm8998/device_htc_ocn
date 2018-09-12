076d35be1bf27820a536db9417147c8799b27896
Nicholas Flintham <nick@flinny.org>
2018-09-12 17:10:46 +0100

    WIP: ocn: shim camera.msm8998.so
    Change-Id: Ib05a8b15b00700de436cacd49b29f74b16ce621d

--- device.mk
+++ device.mk
@@ -162,7 +162,8 @@ PRODUCT_PACKAGES += \
     libmmjpeg_interface \
     libmmcamera_interface \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    libcamera_shim
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
